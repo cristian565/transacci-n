c4d1bcb2763d9eaf4b18fff7807ce3cca03924a2
Cristian565 <cristiancorreajv@unimagdalena.edu.co>
2022-07-20 10:30:34 -0500

    cambio de keycloak a producion

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -2,8 +2,8 @@ import Keycloak from "keycloak-js";
 
 const keycloak =new Keycloak({
     realm: "bizzhub",
-    url: "http://192.168.1.206/auth/", // para local 
-    // url: "https://bizzhub-gateway.hardtech.co:8098/auth/auth", // para vercel-produccion
+   // url: "http://192.168.1.206/auth/", // para local 
+     url: "https://bizzhub-gateway.hardtech.co:8098/auth/auth", // para vercel-produccion
     clientId: "webapp-client",
     PROXY_ADDRESS_FORWARDING: true,
     X_Frame_Options: "SAMEORIGIN"
