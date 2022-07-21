e4bc89c2f142dd6d5be6ba967abf0f0368efb27d
Cristian565 <cristiancorreajv@unimagdalena.edu.co>
2022-07-21 12:59:51 -0500

    prueba perdida de la tabla-2

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -216,9 +216,7 @@ export const Home = () => {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              {
-                dataOrder.transactions
-                  ?
+              
                   <TableTransaction
                     detailsValue={setDetailsValue}
                     openTransaction={setOpenTransaction}
@@ -232,8 +230,7 @@ export const Home = () => {
                     onRefresh={() => mutate()}
                     e2eAttr="table-transaction"
                   />
-                  : <TableTransactionSkeleton e2eAttr="table-transaction" />
-              }
+              
             </Transition>
 
             <Transition show={isLoading}>
@@ -404,16 +401,14 @@ export const Home = () => {
                       <div>
             <Transition
               show={openTransaction && !isLoading && orders?.totalTransactions !== 0}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
+              // enter="transition ease-out duration-100"
+              // enterFrom="opacity-0 translate-y-1"
+              // enterTo="opacity-100 translate-y-0"
+              // leave="transition ease-in duration-150"
+              // leaveFrom="opacity-100 translate-y-0"
+              // leaveTo="opacity-0 translate-y-1"
             >
-              {
-                dataOrder.transactions
-                  ?
+              
                   <TableTransaction
                     detailsValue={setDetailsValue}
                     openTransaction={setOpenTransaction}
@@ -427,8 +422,6 @@ export const Home = () => {
                     onRefresh={() => mutate()}
                     e2eAttr="table-transaction"
                   />
-                  : <TableTransactionSkeleton e2eAttr="table-transaction" />
-              }
             </Transition>
 
             <Transition show={isLoading && orders?.totalTransactions === 0}>
